ed38d41cbaca5d4f391538ecb328f08008e36321
Shawfe Sung <shawfe@techie.com>
2018-10-11 04:22:14 -0400

    Added CORS

---
+++ src/main/java/com/eatsleeppong/ubipong/UbipongRestConfig.java
@@ -0,0 +1,23 @@
+package com.eatsleeppong.ubipong;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class UbipongRestConfig {
+	/**
+	 * this one handles all the plain rest services, annotated with
+	 * @RestController
+	 */
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+}
