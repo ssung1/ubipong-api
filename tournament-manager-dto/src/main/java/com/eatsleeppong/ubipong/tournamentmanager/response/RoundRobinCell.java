7bc1bb3cdb03ece96b0e05538fb20fdcdd091f94
Shawfe Sung <shawfe@techie.com>
2020-12-29 09:04:42 -0500

    Added subproject: tournament-manager-api

---
+++ tournament-manager-dto/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/response/RoundRobinCell.java
@@ -0,0 +1,21 @@
+package com.eatsleeppong.ubipong.tournamentmanager.response;
+
+import lombok.Data;
+
+import java.util.Collections;
+import java.util.List;
+
+@Data
+public class RoundRobinCell {
+    public static final int TYPE_NAME = 10;
+    public static final int TYPE_EMPTY = 11;
+    public static final int TYPE_MATCH_COMPLETE = 12;
+    public static final int TYPE_MATCH_INCOMPLETE = 13;
+    public static final int TYPE_TEXT = 14;
+
+    private int type = TYPE_EMPTY;
+    private String content = "";
+    private boolean winForPlayer1;
+    private boolean winByDefault;
+    private List<Game> gameList = Collections.emptyList();
+}
