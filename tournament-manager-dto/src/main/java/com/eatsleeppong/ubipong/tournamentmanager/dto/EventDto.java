7c70fbb26b8b0f06d928ad481f305989f9712314
Shawfe Sung <shawfe@techie.com>
2021-01-25 08:02:58 -0500

    Made EventDto mutable for now because VSCode is broken

--- tournament-manager-dto/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/dto/EventDto.java
+++ tournament-manager-dto/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/dto/EventDto.java
@@ -1,12 +1,15 @@
 package com.eatsleeppong.ubipong.tournamentmanager.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 @Value
 @Builder
-@Jacksonized
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE) // replace with @Jacksonized after VSCode is fixed
+@AllArgsConstructor // remove after VSCode is fixed
 public class EventDto {
     private Integer id;
     private String challongeUrl;
