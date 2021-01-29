8f1c758545aeaa4a846c191bf40c2968eec3a750
Shawfe Sung <shawfe@techie.com>
2021-01-29 16:00:06 -0500

    Removed commented code

--- tournament-manager-domain/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/domain/Tournament.java
+++ tournament-manager-domain/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/domain/Tournament.java
@@ -18,25 +18,6 @@ public class Tournament {
     private final String name;
     private final Instant tournamentDate;
 
-//    public TournamentResultRequest createTournamentResultRequest(final Integer tournamentId) {
-//        final List<Event> eventList = eventRepository.findByTournamentId(tournamentId);
-//
-//        final TournamentResultRequestLineItem[] tournamentResultRequestLineItemList = eventList.stream()
-//            .map(Event::getChallongeUrl)
-//            .map(eventManager::createTournamentResultList)
-//            .flatMap(Arrays::stream)
-//            .toArray(TournamentResultRequestLineItem[]::new);
-//
-//        final TournamentResultRequest tournamentResultRequest = new TournamentResultRequest();
-//
-//        final Tournament tournament = tournamentRepository.getOne(tournamentId);
-//        tournamentResultRequest.setTournamentName(tournament.getName());
-//        tournamentResultRequest.setTournamentDate(tournament.getTournamentDate());
-//        tournamentResultRequest.setTournamentResultList(tournamentResultRequestLineItemList);
-//
-//        return tournamentResultRequest;
-//    }
-
     public TournamentResult getResult() {
         return TournamentResult.builder()
             .tournamentName(getName())
