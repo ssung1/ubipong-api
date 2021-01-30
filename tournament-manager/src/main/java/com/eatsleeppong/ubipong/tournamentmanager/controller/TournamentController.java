6b39557b6ea3c181c6515f9cb9e50b9fbe54ff5c
Shawfe Sung <shawfe@techie.com>
2021-01-29 19:45:36 -0500

    Use Tournament.getResult

--- tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/controller/TournamentController.java
+++ tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/controller/TournamentController.java
@@ -1,27 +1,31 @@
 package com.eatsleeppong.ubipong.tournamentmanager.controller;
 
 import com.eatsleeppong.ubipong.manager.TournamentManager;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.TournamentRepository;
+import com.eatsleeppong.ubipong.tournamentmanager.mapper.TournamentResultMapper;
 import com.eatsleeppong.ubipong.ratingmanager.dto.TournamentResultDto;
 import com.eatsleeppong.ubipong.tournamentmanager.dto.request.TournamentRequest;
 import com.eatsleeppong.ubipong.tournamentmanager.dto.response.TournamentResponse;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/rest/v0/tournaments")
 public class TournamentController {
-    private final TournamentManager tournamentManager;
-
-    public TournamentController(final TournamentManager tournamentManager) {
-        this.tournamentManager = tournamentManager;
-    }
+    private final TournamentRepository tournamentRepository;
+    private final TournamentResultMapper tournamentResultMapper;
 
     @ApiOperation(value = "Tournament Result", notes = "This is used to generate the tournament report to the rating " +
         "authority after the tournament has ended.  It contains all the matches in the tournament in one big list.")
     @GetMapping(value = "/{id}/result")
     public TournamentResultDto getResult(@PathVariable("id") final Integer id) {
-        return tournamentManager.createTournamentResultRequest(id);
+        return tournamentResultMapper.mapTournamentResultToTournamentResultDto(
+            tournamentRepository.getOne(id).getResult()
+        );
     }
 
     @PostMapping()
