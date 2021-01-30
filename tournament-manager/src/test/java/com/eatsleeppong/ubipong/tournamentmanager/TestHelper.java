96216d482ef71d114fc39e895287c73c668b4fc6
Shawfe Sung <shawfe@techie.com>
2021-01-29 19:36:43 -0500

    Test TournamentRepositoryImpl

---
+++ tournament-manager/src/test/java/com/eatsleeppong/ubipong/tournamentmanager/TestHelper.java
@@ -0,0 +1,135 @@
+package com.eatsleeppong.ubipong.tournamentmanager;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.util.List;
+
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Event;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.EventRepository;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Game;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Match;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.MatchRepository;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Player;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.PlayerRepository;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Tournament;
+
+/**
+ * Tournament Setup:
+ *
+ * Bikini Bottom Open 2019       Jun 23, 2019
+ * Event: Preliminary Group 1 (ID: 100)
+ * Players: spongebob (ID: 1), patrick (ID: 2), and squidward (ID: 3)
+ * Scores:
+ * spongebob vs patrick: patrick wins 3 5 1 (ID: 100)
+ * spongebob vs squidward: spongebob wins 11 -5 9 9 (ID: 101)
+ * patrick vs squidward: not played yet (ID: 102)
+ */
+public class TestHelper {
+    public static final int TOURNAMENT_ID = 10;
+    public static final int SPONGEBOB_ID = 1;
+    public static final int PATRICK_ID = 2;
+    public static final int SQUIDWARD_ID = 3;
+    public static final String CHALLONGE_URL = "esp_201903_pg_rr_1";
+    public static final String EVENT_NAME = "Preliminary Group 1";
+
+    public static Tournament createTournament() {
+        final EventRepository mockEventRepository = mock(EventRepository.class);
+        final Event event = createEvent();
+        when(mockEventRepository.findByTournamentId(TOURNAMENT_ID)).thenReturn(List.of(
+            event
+        ));
+        return Tournament.builder()
+            .id(TOURNAMENT_ID)
+            .eventRepository(mockEventRepository)
+            .name("Eat Sleep Pong Open 2019")
+            .tournamentDate(Instant.parse("2019-03-16T12:00:00Z"))
+            .build();
+    }
+
+    public static Event createEvent() {
+        final PlayerRepository mockPlayerRepository = mock(PlayerRepository.class);
+        final MatchRepository mockMatchRepository = mock(MatchRepository.class);
+
+        when(mockPlayerRepository.findByChallongeUrl(CHALLONGE_URL)).thenReturn(List.of(
+            createPlayerSpongebob(), createPlayerPatrick(), createPlayerSquidward()
+        ));
+
+        when(mockMatchRepository.findByChallongeUrl(CHALLONGE_URL)).thenReturn(List.of(
+            createMatch1(), createMatch2(), createMatch3()
+        ));
+
+        return Event.builder()
+            .id(100)
+            .name("Preliminary Group 1")
+            .challongeUrl(CHALLONGE_URL)
+            .playerRepository(mockPlayerRepository)
+            .matchRepository(mockMatchRepository)
+            .tournamentId(TOURNAMENT_ID)
+            .build();
+    }
+
+    public static Player createPlayerSpongebob() {
+        return Player.builder()
+            .id(SPONGEBOB_ID)
+            .name("spongebob")
+            .build();
+    }
+
+    public static Player createPlayerPatrick() {
+        return Player.builder()
+            .id(PATRICK_ID)
+            .name("patrick")
+            .build();
+    }
+
+    public static Player createPlayerSquidward() {
+        return Player.builder()
+            .id(SQUIDWARD_ID)
+            .name("patrick")
+            .build();
+    }
+
+    public static Match createMatch1() {
+        return Match.builder()
+            .id(1000)
+            .player1Id(SPONGEBOB_ID)
+            .player2Id(PATRICK_ID)
+            .status(Match.STATUS_COMPLETE)
+            .resultCode(Match.RESULT_CODE_WIN_BY_PLAYING)
+            .winnerId(PATRICK_ID)
+            .gameList(List.of(
+                Game.builder().scores("3-11").build(),
+                Game.builder().scores("5-11").build(),
+                Game.builder().scores("1-11").build()
+            ))
+            .build();
+    }
+
+    public static Match createMatch2() {
+        return Match.builder()
+            .id(1001)
+            .player1Id(SPONGEBOB_ID)
+            .player2Id(SQUIDWARD_ID)
+            .status(Match.STATUS_COMPLETE)
+            .resultCode(Match.RESULT_CODE_WIN_BY_PLAYING)
+            .winnerId(SPONGEBOB_ID)
+            .gameList(List.of(
+                Game.builder().scores("13-11").build(),
+                Game.builder().scores("5-11").build(),
+                Game.builder().scores("11-9").build(),
+                Game.builder().scores("11-9").build()
+            ))
+            .build();
+    }
+
+    public static Match createMatch3() {
+        return Match.builder()
+            .id(1001)
+            .player1Id(PATRICK_ID)
+            .player2Id(SQUIDWARD_ID)
+            .build();
+    }
+
+}
---
+++ tournament-manager/src/test/java/com/eatsleeppong/ubipong/tournamentmanager/repository/TestTournamentRepositoryImpl.java
@@ -0,0 +1,61 @@
+package com.eatsleeppong.ubipong.tournamentmanager.repository;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.Date;
+
+import com.eatsleeppong.ubipong.entity.SpringJpaTournament;
+import com.eatsleeppong.ubipong.tournamentmanager.TestHelper;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Tournament;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class TestTournamentRepositoryImpl {
+
+    /**
+     * just a helper to insert SpringJpaTournament into database
+     */
+    @Autowired
+    private SpringJpaTournamentRepository springJpaTournamentRepository;
+
+    @Autowired
+    private TournamentRepositoryImpl tournamentRepository;
+
+    /**
+     * remove later when we do not use SpringJpaTournament directly
+     */
+    private SpringJpaTournament createSpringJpaTournament() {
+        final Tournament tournament = TestHelper.createTournament();
+        final SpringJpaTournament springJpaTournament = new SpringJpaTournament();
+        springJpaTournament.setName(tournament.getName());
+        springJpaTournament.setTournamentDate(Date.from(tournament.getTournamentDate()));
+        return springJpaTournament;
+    }
+
+    @Test
+    @DisplayName("should return a tournament by tournament ID")
+    public void testGetOne() {
+        final SpringJpaTournament springJpaTournamentToAdd = createSpringJpaTournament();
+        final SpringJpaTournament addedSpringJpaTournament =
+            springJpaTournamentRepository.save(springJpaTournamentToAdd);
+        final Integer tournamentId = addedSpringJpaTournament.getId();
+
+        final Tournament tournament = tournamentRepository.getOne(tournamentId);
+
+        assertThat(tournament, notNullValue());
+        assertThat(tournament.getName(), is(springJpaTournamentToAdd.getName()));
+        assertThat(tournament.getTournamentDate(), is(springJpaTournamentToAdd.getTournamentDate().toInstant()));
+    }
+}
