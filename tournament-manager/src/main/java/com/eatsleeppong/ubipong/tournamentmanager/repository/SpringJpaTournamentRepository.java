ad708b58677b09947df897d5b3cf54c0d0e8f20e
Shawfe Sung <shawfe@techie.com>
2021-03-16 09:12:24 -0400

    Turned back on crud for tournament because we need paging

--- tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/repository/SpringJpaTournamentRepository.java
+++ tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/repository/SpringJpaTournamentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-@RepositoryRestResource(collectionResourceRel = "tournaments", path = "tournaments", exported = false)
+@RepositoryRestResource(collectionResourceRel = "tournaments", path = "tournaments")
 public interface SpringJpaTournamentRepository extends JpaRepository<SpringJpaTournament, Integer> {
     Optional<SpringJpaTournament> findByName(@Param("name") String name);
 }
