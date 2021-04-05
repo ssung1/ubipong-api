0b5191abf94517c3820e9a5a82490b35c736b392
Shawfe Sung <shawfe@techie.com>
2021-04-05 08:49:31 -0400

    Added GameMapper and RoundRobinCellMapper

---
+++ tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/mapper/GameMapper.java
@@ -0,0 +1,17 @@
+package com.eatsleeppong.ubipong.tournamentmanager.mapper;
+
+import com.eatsleeppong.ubipong.tournamentmanager.dto.GameDto;
+import com.eatsleeppong.ubipong.tournamentmanager.domain.Game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameMapper {
+    public GameDto mapGameToGameDto(final Game game) {
+        return GameDto.builder()
+            .player1Score(game.getPlayer1Score())
+            .player2Score(game.getPlayer2Score())
+            .winForPlayer1(game.isWinForPlayer1())
+            .build();
+    }
+}
---
+++ tournament-manager/src/main/java/com/eatsleeppong/ubipong/tournamentmanager/mapper/RoundRobinCellMapper.java
@@ -0,0 +1,28 @@
+package com.eatsleeppong.ubipong.tournamentmanager.mapper;
+
+import java.util.stream.Collectors;
+
+import com.eatsleeppong.ubipong.tournamentmanager.domain.RoundRobinCell;
+import com.eatsleeppong.ubipong.tournamentmanager.dto.RoundRobinCellDto;
+import com.eatsleeppong.ubipong.tournamentmanager.dto.RoundRobinCellTypeDto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class RoundRobinCellMapper {
+    private final GameMapper gameMapper;
+
+    public RoundRobinCellDto mapRoundRobinCellToRoundRobinCellDto(final RoundRobinCell roundRobinCell) {
+        return RoundRobinCellDto.builder()
+            .type(RoundRobinCellTypeDto.valueOf(roundRobinCell.getType().name()))
+            .content(roundRobinCell.getContent())
+            .winForPlayer1(roundRobinCell.isWinForPlayer1())
+            .winByDefault(roundRobinCell.isWinByDefault())
+            .gameList(roundRobinCell.getGameList().stream()
+                .map(gameMapper::mapGameToGameDto).collect(Collectors.toUnmodifiableList()))
+            .build();
+    }
+}
